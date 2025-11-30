506204dcfb3cfacbd63bfae653002a4a000f339c
thy3368 <thy_email@126.com>
2025-12-01 00:55:32 +0800

    prep,spot 两种

---
+++ lib/core/prep/src/domain/mod.rs
@@ -0,0 +1,8 @@
+//! Perpetual contract domain layer
+//!
+//! Contains entities, value objects and domain services
+
+pub mod entity;
+pub mod service;
+
+pub use service::*;
---
+++ lib/core/prep/src/domain/service/mod.rs
@@ -0,0 +1,7 @@
+//! Perpetual contract domain services
+//!
+//! Contains command definitions and handlers
+
+pub mod command;
+
+pub use command::*;
