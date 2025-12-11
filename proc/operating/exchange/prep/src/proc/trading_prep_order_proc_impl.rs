0ae9cdde95fc5bb23e171b512ed26d50eee11cbd
thy3368 <thy_email@126.com>
2025-12-11 10:52:47 +0800

    prep base command design

--- proc/operating/exchange/prep/src/proc/trading_prep_order_proc_impl.rs
+++ proc/operating/exchange/prep/src/proc/trading_prep_order_proc_impl.rs
@@ -1,7 +1,7 @@
-use crate::proc::trading_prep_order_proc::{CancelAllOrdersCommand, CancelAllOrdersResult, CancelOrderCommand, CancelOrderResult, ClosePositionCommand, ClosePositionResult, ModifyOrderCommand, ModifyOrderResult, OpenPositionCommand, OpenPositionResult, OrderBookSnapshot, OrderQueryResult, PerpOrderExchangeProc, PositionInfo, PrepCommandError, QueryOrderBookCommand, QueryOrderCommand, QueryPositionCommand, QueryTradesCommand, TradesQueryResult};
+use crate::proc::trading_prep_order_proc::{AccountBalance, AccountInfo, CancelAllOrdersCommand, CancelAllOrdersResult, CancelOrderCommand, CancelOrderResult, ClosePositionCommand, ClosePositionResult, MarkPriceInfo, ModifyOrderCommand, ModifyOrderResult, OpenPositionCommand, OpenPositionResult, OrderBookSnapshot, OrderQueryResult, PerpOrderExchangeProc, PositionInfo, PrepCommandError, QueryAccountBalanceCommand, QueryAccountInfoCommand, QueryMarkPriceCommand, QueryOrderBookCommand, QueryOrderCommand, QueryPositionCommand, QueryTradesCommand, SetLeverageCommand, SetLeverageResult, SetMarginTypeCommand, SetMarginTypeResult, SetPositionModeCommand, SetPositionModeResult, TradesQueryResult};
 
 pub struct MatchingService {
-    
+
 }
 
 impl PerpOrderExchangeProc for MatchingService {
@@ -38,6 +38,30 @@ impl PerpOrderExchangeProc for MatchingService {
     }
 
     fn query_trades(&self, cmd: QueryTradesCommand) -> Result<TradesQueryResult, PrepCommandError> {
+        todo!()
+    }
+
+    fn set_leverage(&mut self, cmd: SetLeverageCommand) -> Result<SetLeverageResult, PrepCommandError> {
+        todo!()
+    }
+
+    fn set_margin_type(&mut self, cmd: SetMarginTypeCommand) -> Result<SetMarginTypeResult, PrepCommandError> {
+        todo!()
+    }
+
+    fn set_position_mode(&mut self, cmd: SetPositionModeCommand) -> Result<SetPositionModeResult, PrepCommandError> {
+        todo!()
+    }
+
+    fn query_account_balance(&self, cmd: QueryAccountBalanceCommand) -> Result<Vec<AccountBalance>, PrepCommandError> {
+        todo!()
+    }
+
+    fn query_account_info(&self, cmd: QueryAccountInfoCommand) -> Result<AccountInfo, PrepCommandError> {
+        todo!()
+    }
+
+    fn query_mark_price(&self, cmd: QueryMarkPriceCommand) -> Result<Vec<MarkPriceInfo>, PrepCommandError> {
         todo!()
     }
 }
