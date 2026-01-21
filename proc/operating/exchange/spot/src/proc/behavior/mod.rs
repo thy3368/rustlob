6cf8fb57faee36776c05215a0603b3329c44f6b8
thy3368 <thy_email@126.com>
2026-01-21 11:30:02 +0800

    重构

--- proc/operating/exchange/spot/src/proc/behavior/mod.rs
+++ proc/operating/exchange/spot/src/proc/behavior/mod.rs
@@ -1,10 +1,8 @@
 // 订单命令处理器trait
 
 // 仓储接口和实现
-pub mod order_query_behavior;
 pub mod id_repo;
 
+pub mod spot_market_data_behavior;
 pub mod spot_trade_behavior;
 pub mod spot_user_data_behavior;
-pub mod spot_market_data_behavior;
-
