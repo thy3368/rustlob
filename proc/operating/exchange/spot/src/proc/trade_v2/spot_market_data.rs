6b736d198fba218a34a23a8fbd86d4ffcc785009
thy3368 <thy_email@126.com>
2026-01-22 17:06:52 +0800

    重构

--- proc/operating/exchange/spot/src/proc/trade_v2/spot_market_data.rs
+++ proc/operating/exchange/spot/src/proc/trade_v2/spot_market_data.rs
@@ -1,4 +1,27 @@
+use crate::proc::behavior::spot_trade_behavior::SpotCmdError;
+use crate::proc::behavior::v2::spot_market_data_behavior::{
+    SpotMarketDataBehavior, SpotMarketDataCmdAny, SpotMarketDataRes,
+};
+use base_types::cqrs::cqrs_types::CmdResp;
 
-pub struct SpotMarketDataImpl {
+pub struct SpotMarketDataImpl {}
 
+impl SpotMarketDataBehavior for SpotMarketDataImpl {
+    fn handle(&mut self, cmd: SpotMarketDataCmdAny) -> Result<CmdResp<SpotMarketDataRes>, SpotCmdError> {
+        match cmd {
+            SpotMarketDataCmdAny::OrderBook(_) => {}
+            SpotMarketDataCmdAny::RecentTrades(_) => {}
+            SpotMarketDataCmdAny::HistoricalTrades(_) => {}
+            SpotMarketDataCmdAny::AggTrades(_) => {}
+            SpotMarketDataCmdAny::Klines(_) => {}
+            SpotMarketDataCmdAny::UIKlines(_) => {}
+            SpotMarketDataCmdAny::AvgPrice(_) => {}
+            SpotMarketDataCmdAny::Ticker24hr(_) => {}
+            SpotMarketDataCmdAny::TradingDayTicker(_) => {}
+            SpotMarketDataCmdAny::SymbolPriceTicker(_) => {}
+            SpotMarketDataCmdAny::BookTicker(_) => {}
+            SpotMarketDataCmdAny::RollingWindowTicker(_) => {}
+        }
+        todo!()
+    }
 }
