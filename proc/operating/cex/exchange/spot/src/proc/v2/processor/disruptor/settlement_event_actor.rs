b61f7b82ffe3ac9ec65984a6af3f1018f46943fb
thy3368 <thy_email@126.com>
2026-04-08 20:16:20 +0800

    update

--- proc/operating/cex/exchange/spot/src/proc/v2/processor/disruptor/settlement_event_actor.rs
+++ proc/operating/cex/exchange/spot/src/proc/v2/processor/disruptor/settlement_event_actor.rs
@@ -1,17 +1,19 @@
 use base_types::exchange::spot::spot_types::SpotTrade;
 use crossbeam_channel::Receiver;
+use db_repo::core::db_repo2::CmdRepo2;
+use db_repo::core::event_publish::EventPublisher2;
 use diff::diff_types::DomainEvent;
 
 use crate::proc::v2::processor::disruptor::inproc_event_actor::InprocEventActor;
 use crate::proc::v2::trade_cmd_handlers::v3::event_handler::new_trade_event_handler::NewTradeEventHandler;
 
-pub type DisruptorSettlementEventActor =
-    InprocEventActor<DomainEvent<SpotTrade>, NewTradeEventHandler>;
+pub type DisruptorSettlementEventActor<R, P> =
+    InprocEventActor<DomainEvent<SpotTrade>, NewTradeEventHandler<R, P>>;
 
-impl DisruptorSettlementEventActor {
+impl<R: CmdRepo2, P: EventPublisher2> DisruptorSettlementEventActor<R, P> {
     pub fn from_parts(
         receiver: Receiver<DomainEvent<SpotTrade>>,
-        handler: std::sync::Arc<NewTradeEventHandler>,
+        handler: std::sync::Arc<NewTradeEventHandler<R, P>>,
     ) -> Self {
         InprocEventActor::new(receiver, handler, "disruptor-settlement-event-actor")
     }
--- proc/operating/cex/exchange/spot/src/proc/v2/processor/nats/matching_event_actor.rs
+++ proc/operating/cex/exchange/spot/src/proc/v2/processor/nats/matching_event_actor.rs
@@ -1,18 +1,24 @@
 use base_types::exchange::spot::spot_types::SpotOrder;
+use base_types::lob::lob::LobOrder;
+use db_repo::core::db_repo2::CmdRepo2;
+use db_repo::core::event_publish::EventPublisher2;
 use diff::diff_types::DomainEvent;
+use lob_repo::core::symbol_lob_repo::MultiSymbolLobRepo;
 
 use crate::proc::behavior::spot_trade_behavior::SpotCmdErrorAny;
 use crate::proc::v2::processor::nats::nats_event_actor::{NatsEventActor, NatsProcessorConfig};
 use crate::proc::v2::trade_cmd_handlers::v3::event_handler::new_order_place_event_handler::NewOrderPlaceEventHandler;
 
-pub type NatsMatchingEventActor =
-    NatsEventActor<DomainEvent<SpotOrder>, NewOrderPlaceEventHandler>;
+pub type NatsMatchingEventActor<R, P, L> =
+    NatsEventActor<DomainEvent<SpotOrder>, NewOrderPlaceEventHandler<R, P, L>>;
 
-impl NatsMatchingEventActor {
+impl<R: CmdRepo2, P: EventPublisher2, L: MultiSymbolLobRepo<Order = SpotOrder>>
+    NatsMatchingEventActor<R, P, L>
+{
     pub fn from_parts(
         config: NatsProcessorConfig,
         subject: String,
-        handler: std::sync::Arc<NewOrderPlaceEventHandler>,
+        handler: std::sync::Arc<NewOrderPlaceEventHandler<R, P, L>>,
     ) -> Result<Self, String> {
         NatsEventActor::new(
             config,
@@ -27,9 +33,9 @@ impl NatsMatchingEventActor {
 fn deserialize_matching_domain_event(
     bytes: &[u8],
 ) -> Result<DomainEvent<SpotOrder>, SpotCmdErrorAny> {
-    serde_json::from_slice(bytes).map_err(|e| SpotCmdErrorAny::Common(
-        crate::proc::behavior::spot_trade_behavior::CommonError::Internal {
+    serde_json::from_slice(bytes).map_err(|e| {
+        SpotCmdErrorAny::Common(crate::proc::behavior::spot_trade_behavior::CommonError::Internal {
             message: format!("Deserialization error: {}", e),
-        },
-    ))
+        })
+    })
 }
