f1997272ecc2ec46e0247aad0a2ae25ff0acefdc
thy3368 <thy_email@126.com>
2026-06-09 10:48:57 +0800

    update

--- lib/example/core/src/use_case/trading/derivatives/hyperliquid_perp/mod.rs
+++ lib/example/core/src/use_case/trading/derivatives/hyperliquid_perp/mod.rs
@@ -1,17 +1,17 @@
-pub mod match_hyperliquid_perp_order;
-pub mod place_hyperliquid_perp_order;
-pub mod settle_hyperliquid_perp_trade;
+pub mod match_perp_order;
+pub mod place_perp_order;
+pub mod settle_perp_trade;
 
-pub use match_hyperliquid_perp_order::{
+pub use match_perp_order::{
     MatchHyperliquidPerpOrderCmd, MatchHyperliquidPerpOrderError, MatchHyperliquidPerpOrderState,
     MatchHyperliquidPerpOrderUseCase,
 };
-pub use place_hyperliquid_perp_order::{
+pub use place_perp_order::{
     PlaceHyperliquidPerpOrderCmd, PlaceHyperliquidPerpOrderError,
     PlaceHyperliquidPerpOrderExecution, PlaceHyperliquidPerpOrderState,
     PlaceHyperliquidPerpOrderUseCase,
 };
-pub use settle_hyperliquid_perp_trade::{
+pub use settle_perp_trade::{
     SettleHyperliquidPerpTradeCmd, SettleHyperliquidPerpTradeError,
     SettleHyperliquidPerpTradeState, SettleHyperliquidPerpTradeUseCase,
 };
