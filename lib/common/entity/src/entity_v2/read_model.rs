ef4822407b2af2f3d5bad2bfc0b043c0a0e01326
thy3368 <thy_email@126.com>
2026-07-28 11:03:00 +0800

    update

--- lib/common/entity/src/entity_v2/read_model.rs
+++ lib/common/entity/src/entity_v2/read_model.rs
@@ -4,7 +4,7 @@
 /// 只承载已装配事实、快照或派生查询状态，不作为命令写侧 aggregate 使用。
 pub trait DomainReadModel {}
 
-/// 领域快照标识。
+/// 领域读快照标识。
 ///
 /// 表示该读模型是某个业务时点上已装配事实与派生状态的截面。
 pub trait DomainReadSnapshot: DomainReadModel {}
