27d1ffa58bb65f7a690c32d96a0dd8a45b9e982e
thy3368 <thy_email@126.com>
2026-09-18 22:39:39 +0800

    update

--- lib/example/veldra_inbound_adapter/src/command/exchange/mod.rs
+++ lib/example/veldra_inbound_adapter/src/command/exchange/mod.rs
@@ -8,9 +8,6 @@ mod spec_coverage;
 #[cfg(test)]
 mod test_support;
 
-pub use actions::batch_modify::{
-    BatchModifyCancelPlaceExecutor, run_batch_modify_cancel_replace_with_executor,
-};
 pub use actions::cancel::reply::CancelStatusWire;
 pub use actions::cancel::{CancelSpotOrderV2Lookup, CancelSpotOrderV2Request};
 pub use actions::order::PlaceSpotOrderV2Request;
